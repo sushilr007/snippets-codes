f52ecfd30adfac8375979851e2fa70d48599d6c3
Sheel <35765579+sushilr007@users.noreply.github.com>
2018-04-12 18:39:20 +0530

    Update security.js
    Adding connection label response.

--- nodejs/security.js
+++ nodejs/security.js
@@ -2,3 +2,7 @@
 var string = bundle.authData.password; //bundle.authData.password is a (string).
   var buffer = new Buffer(string); //Creating a buffer.
   var toBase64 = buffer.toString('base64'); //converting a string to base64
+
+
+**Returning multiple values as connection label in zapier.
+return ' {{bundle.authData.subdomain}} | {{bundle.authData.username}}' // It will send Domain and username in zapier field. 
