acd3d5c8f1698a1ff4567813b4b197506d738118
Sheel <35765579+sushilr007@users.noreply.github.com>
2018-04-12 18:52:15 +0530

    Create functions.js
    functions used in node js

---
+++ nodejs/functions.js
@@ -0,0 +1,6 @@
+//Checking if it array or not
+Array.isArray(response["json"]["ms_response"]["tasks"]["task"])
+
+
+//It will check whether its "Not A Number"
+Number.isNaN(bundle.inputData.project_id)
