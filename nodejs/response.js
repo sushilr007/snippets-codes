236a2e55ced91cbce848aa9b5e810afe1fec2928
Sheel <35765579+sushilr007@users.noreply.github.com>
2018-04-12 18:48:59 +0530

    Update response.js
    Response as selective fields.

--- nodejs/response.js
+++ nodejs/response.js
@@ -7,9 +7,28 @@ var ob =  JSON.parse(response.content); //accepting response as string and conve
            z.console.log ("p1",response);
            return bb; // returning array present in "bb" 
            
-//normal returning using json parse.
+**//normal returning using json parse.
 return promise.then((response) => JSON.parse(response.content));
 
-//returning response in array.
+**//returning response in array.
 return response["json"]["ms_response"]["xyz"];
+
+**//Getting response as array and sending that response as object (Selective field)
+var task = response["json"]["ms_response"]["tasks"]["task"];
+var taskRes = [ //selective response
+   {
+      "id": task[0]["id"],
+      "accepted_on": task[0]["accepted_on"],
+   }]
+return taskRes;
+
+**//Getting response as string and sending as res as object (Selective fields)
+var task = JSON.parse(response.content);
+                  var taskres = [ //selective response
+                     {
+                        
+                        "id": task.ms_response.tasks.task.id,
+                        "accepted_on": task.ms_response.tasks.task.accepted_on,
+                     }];
+                  return taskres;
            
