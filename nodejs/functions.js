5b53027f2eb596ae43cb1fd6265b0d403a498323
Sheel <35765579+sushilr007@users.noreply.github.com>
2018-06-27 18:45:26 +0530

    Update functions.js

--- nodejs/functions.js
+++ nodejs/functions.js
@@ -4,3 +4,7 @@ Array.isArray(response["json"]["ms_response"]["tasks"]["task"])
 
 //It will check whether its "Not A Number"
 Number.isNaN(bundle.inputData.project_id)
+
+//It will remove "null,undefined" from array.
+var _ = require('lodash');
+_.compact(array);
